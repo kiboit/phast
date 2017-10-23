d7a424a8c85004ecd56b8b2bb65f38f1ea8986c5
Milko Kosturkov <milko@kiboit.com>
2017-10-23 15:28:02 +0200

    Create css inlining HTML filter

---
+++ src/Filters/CSSInliningHTMLFilter.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Kibo\Phast\Filters;
+
+class CSSInliningHTMLFilter implements HTMLFilter {
+
+    /**
+     * @var string
+     */
+    private $baseURL;
+
+    /**
+     * @var callable
+     */
+    private $retrieveFile;
+
+    public function __construct($baseURL, callable $fileRetrieverCallback) {
+        $this->baseURL = $baseURL;
+        $this->retrieveFile = $fileRetrieverCallback;
+    }
+
+    public function transformHTMLDOM(\DOMDocument $document) {
+        $links = iterator_to_array($document->getElementsByTagName('link'));
+        foreach ($links as $link) {
+            if ($this->shouldInline($link)) {
+                $this->inline($link, $document);
+            }
+        }
+    }
+
+    private function shouldInline(\DOMElement $link) {
+        return  $link->hasAttribute('rel')
+                && $link->getAttribute('rel') == 'stylesheet'
+                && $link->hasAttribute('href')
+                && !$this->isCrossSiteUrl($link->getAttribute('href'));
+    }
+
+    private function inline(\DOMElement $link, \DOMDocument $document) {
+        $location = $link->getAttribute('href');
+        $content = @call_user_func($this->retrieveFile, $location);
+        if ($content === false) {
+            return;
+        }
+        $style = $document->createElement('style');
+        $style->textContent = $this->rewriteRelativeURLs($content, $location);
+        if ($link->hasAttribute('media')) {
+            $style->setAttribute('media', $link->getAttribute('media'));
+        }
+        $link->parentNode->insertBefore($style, $link);
+        $link->parentNode->removeChild($link);
+    }
+
+    /**
+     * @param string $url
+     * @return bool
+     */
+    private function isCrossSiteUrl($url) {
+        $urlComponents = parse_url($url);
+        return !empty ($urlComponents['host']) && $urlComponents['host'] != $this->baseURL;
+    }
+
+    private function rewriteRelativeURLs($cssContent, $cssUrl) {
+        $cssUrlComponents = parse_url($cssUrl);
+        $absBase = $this->compileUrl(array_merge($cssUrlComponents, [
+            'path' => '/',
+            'query' => null,
+            'fragment' => null,
+        ]));
+
+        $relBase = $this->compileUrl(array_merge($cssUrlComponents, [
+            'path' => preg_replace('~[^/]*$~', '', $cssUrlComponents['path']),
+            'query' => null,
+            'fragment' => null,
+        ]));
+
+        return preg_replace_callback(
+            '~
+                (
+                    @import \s+ ["\'] |
+                    url\( \s* (?:"|\'|)
+                )
+                (?! (?:[a-z]+:)? // )
+                ([A-Za-z0-9_/.-])
+            ~xi',
+            function ($match) use ($absBase, $relBase) {
+                $base = $match[2] == '/' ? $absBase : $relBase;
+                return $match[1] . $base . $match[2];
+            },
+            $cssContent
+        );
+    }
+
+    private function compileUrl(array $urlComponents) {
+        $scheme   = isset($urlComponents['scheme']) ? $urlComponents['scheme'] . '://' : '';
+        $host     = isset($urlComponents['host']) ? $urlComponents['host'] : '';
+        $port     = isset($urlComponents['port']) ? ':' . $urlComponents['port'] : '';
+        $user     = isset($urlComponents['user']) ? $urlComponents['user'] : '';
+        $pass     = isset($urlComponents['pass']) ? ':' . $urlComponents['pass']  : '';
+        $pass     = ($user || $pass) ? "$pass@" : '';
+        $path     = isset($urlComponents['path']) ? $urlComponents['path'] : '';
+        $query    = isset($urlComponents['query']) ? '?' . $urlComponents['query'] : '';
+        $fragment = isset($urlComponents['fragment']) ? '#' . $urlComponents['fragment'] : '';
+        return "$scheme$user$pass$host$port$path$query$fragment";
+    }
+
+
+
+}
---
+++ test/Filters/CSSInliningHTMLFilterTest.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace Kibo\Phast\Filters;
+
+use PHPUnit\Framework\TestCase;
+
+class CSSInliningHTMLFilterTest extends TestCase {
+
+    const BASE_URL = 'kibo-test.org';
+
+    private $files;
+
+    /**
+     * @var CSSInliningHTMLFilter
+     */
+    private $filter;
+
+    /**
+     * @var \DOMDocument
+     */
+    private $dom;
+
+    /**
+     * @var \DOMElement
+     */
+    private $html;
+
+    /**
+     * @var \DOMElement
+     */
+    private $head;
+
+    /**
+     * @var \DOMElement
+     */
+    private $body;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->dom = new \DOMDocument();
+        $this->html = $this->dom->createElement('html');
+        $this->dom->appendChild($this->html);
+        $this->head = $this->dom->createElement('head');
+        $this->html->appendChild($this->head);
+        $this->body = $this->dom->createElement('body');
+        $this->html->appendChild($this->body);
+
+        $this->files = [];
+        $retrieveFile = function ($filename) {
+            return isset ($this->files[$filename]) ? $this->files[$filename] : false;
+        };
+        $this->filter = new CSSInliningHTMLFilter(self::BASE_URL, $retrieveFile);
+    }
+
+    public function testInliningCSS() {
+
+        $this->makeLink($this->head, 'the-file-contents');
+        $this->makeLink($this->body, 'the-file-2-contents');
+
+        $this->body->appendChild(
+            $this->dom->createElement('div')
+        );
+
+        $this->filter->transformHTMLDOM($this->dom);
+
+
+        $styles = $this->getTheStyles();
+
+        $this->assertCount(2, $styles);
+        $this->assertEquals('the-file-contents', $styles[0]->textContent);
+        $this->assertEquals('the-file-2-contents', $styles[1]->textContent);
+        $this->assertSame($this->head, $styles[0]->parentNode);
+        $this->assertSame($this->body, $styles[1]->parentNode);
+        $this->assertSame($this->body->childNodes[0], $styles[1]);
+    }
+
+    public function testKeepingMediaAttribute() {
+        $link = $this->makeLink($this->head);
+        $link->setAttribute('media', 'print');
+
+        $this->filter->transformHTMLDOM($this->dom);
+
+        $styles = $this->getTheStyles();
+        $this->assertCount(1, $styles);
+        $this->assertTrue($styles[0]->hasAttribute('media'));
+        $this->assertEquals('print', $styles[0]->getAttribute('media'));
+    }
+
+    public function testInliningWithCorrectRel() {
+        $badRel = $this->makeLink($this->head);
+        $noRel = $this->makeLink($this->head);
+        $noHref = $this->makeLink($this->head);
+        $crossSite = $this->makeLink($this->head);
+
+        $badRel->setAttribute('rel', 'not-style');
+        $noRel->removeAttribute('rel');
+        $noHref->removeAttribute('href');
+        $crossSite->setAttribute('href', 'http://www.example.com/some-file.css');
+
+        $this->filter->transformHTMLDOM($this->dom);
+
+        $this->assertEmpty($this->getTheStyles());
+        $this->assertSame($badRel, $this->head->childNodes[0]);
+        $this->assertSame($noRel, $this->head->childNodes[1]);
+        $this->assertSame($noHref, $this->head->childNodes[2]);
+        $this->assertSame($crossSite, $this->head->childNodes[3]);
+    }
+
+    public function testCSSContentsRelativeURLsRewriting() {
+        $absoluteUrl = 'http://' . self::BASE_URL . '/style.css';
+        $rootUrl = '/style.css';
+        $relativeUrl = 'style.css';
+        $crossSiteUrl = 'http://cross-site.org/css/style.css';
+
+        $this->makeLink($this->head, "@import '$absoluteUrl'");
+        $this->makeLink($this->head, "url('$absoluteUrl')");
+        $this->makeLink($this->head, "@import \"$rootUrl\"", '/css/sheet1.css');
+        $this->makeLink($this->head, "url(\"$rootUrl\")", '/css/sheet2.css');
+        $this->makeLink($this->head, "@import '$relativeUrl''", '/css/sheet4.css');
+        $this->makeLink($this->head, "url('$relativeUrl')", '/css/sheet5.css');
+        $this->makeLink($this->head, "@import '$crossSiteUrl'", '/css/sheet6.css');
+        $this->makeLink($this->head, "url('$crossSiteUrl')", '/css/sheet7.css');
+
+
+        $this->filter->transformHTMLDOM($this->dom);
+
+        $styles = $this->getTheStyles();
+
+        $this->assertEquals("@import '$absoluteUrl'", $styles[0]->textContent);
+        $this->assertEquals("url('$absoluteUrl')", $styles[1]->textContent);
+        $this->assertEquals("@import \"/$rootUrl\"", $styles[2]->textContent);
+        $this->assertEquals("url(\"/$rootUrl\")", $styles[3]->textContent);
+        $this->assertEquals("@import '/css/$relativeUrl''", $styles[4]->textContent);
+        $this->assertEquals("url('/css/$relativeUrl')", $styles[5]->textContent);
+        $this->assertEquals("@import '$crossSiteUrl'", $styles[6]->textContent);
+        $this->assertEquals("url('$crossSiteUrl')", $styles[7]->textContent);
+    }
+
+    public function testNotInliningOnReadError() {
+        $theLink = $this->makeLink($this->head);
+        $retrieve = function () {
+            trigger_error('An error', E_USER_WARNING);
+            return false;
+        };
+        $filter = new CSSInliningHTMLFilter(self::BASE_URL, $retrieve);
+        $filter->transformHTMLDOM($this->dom);
+
+        $this->assertEmpty($this->getTheStyles());
+        $this->assertSame($this->head->childNodes[0], $theLink);
+    }
+
+    /**
+     * @param \DOMElement $parent
+     * @param string $content
+     * @param string|null $url
+     * @return \DOMElement
+     */
+    private function makeLink(\DOMElement $parent, $content = 'some-content', $url = null) {
+        static $nextFileIndex = 0;
+        $fileName = is_null($url) ? '/css-file-' . $nextFileIndex++ : $url;
+        $link = $this->dom->createElement('link');
+        $link->setAttribute('href', $fileName);
+        $link->setAttribute('rel', 'stylesheet');
+        $parent->appendChild($link);
+
+        $this->files[$fileName] = $content;
+        return $link;
+    }
+
+    /**
+     * @return \DOMElement[]
+     */
+    private function getTheStyles() {
+        return iterator_to_array($this->dom->getElementsByTagName('style'));
+    }
+
+
+}
