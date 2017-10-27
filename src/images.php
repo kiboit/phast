6ddd1f33b86639fbff5c5d9bdad89fb0cbbbed27
Milko Kosturkov <milko@kiboit.com>
2017-10-27 14:53:13 +0200

    Redirect to original image in any type of error

--- src/images.php
+++ src/images.php
@@ -1,20 +1,18 @@
 <?php
+if (isset ($_GET['src']) && !headers_sent())  {
+    header('Location: ' . $_GET['src']);
+} else {
+    http_response_code(404);
+    exit;
+}
+
 require_once __DIR__ . '/bootstrap.php';
 $config = require_once PHAST_CONFIG_FILE;
-try {
-    $image = \Kibo\Phast\Factories\ImageFilteringServiceFactory::make($config)->serve($_GET);
-    header('Content-Type: ' . $image->getType());
-    header('Cache-Control: max-age=' . (86400 * 365));
-    echo $image->getAsString();
-} catch (\Kibo\Phast\Exceptions\ItemNotFoundException $e) {
-    $url = $e->getUrl();
-    if ($url) {
-        header('Location: ' . $url);
-    } else {
-        http_response_code(404);
-    }
-} catch (\Kibo\Phast\Exceptions\UnauthorizedException $e) {
-    http_response_code(403);
-} catch (Exception $e) {
-    http_response_code(500);
-}
+$image = \Kibo\Phast\Factories\ImageFilteringServiceFactory::make($config)->serve($_GET);
+$output = $image->getAsString();
+
+header_remove('Location');
+header('Content-Type: ' . $image->getType());
+header('Cache-Control: max-age=' . (86400 * 365));
+
+echo $output;
