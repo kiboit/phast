cc135cfd917d3fb173e662d5c874c638422f99c8
Albert Peschar <albert@peschar.net>
2017-10-30 17:57:48 +0100

    LocalRetriever: Support symlinks outside docroot

--- src/Retrievers/LocalRetriever.php
+++ src/Retrievers/LocalRetriever.php
@@ -28,11 +28,11 @@ class LocalRetriever implements Retriever {
             $this->retrieveCb = $retrieveCb;
         } else {
             $this->retrieveCb = function ($file, $base) {
-                $base = realpath($base) . '/';
+                $base = $this->normalizePath($base) . '/';
                 if (!$base) {
                     return false;
                 }
-                $path = realpath($base . $file);
+                $path = $this->normalizePath($base . $file);
                 if (!$path) {
                     return false;
                 }
@@ -50,6 +50,27 @@ class LocalRetriever implements Retriever {
         }
         $base = URL::fromString($this->map[$url->getHost()]);
         return call_user_func($this->retrieveCb, $url->getPath(), $base);
+    }
+
+    private function normalizePath($path) {
+        $path = explode("\0", $path)[0];
+        $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
+        $absolutes = array();
+        $parts = explode(DIRECTORY_SEPARATOR, $path);
+        if ($parts[0] == '') {
+            $absolutes[] = '';
+        }
+        foreach ($parts as $part) {
+            if ('.' == $part || '' == $part) {
+                continue;
+            }
+            if ('..' == $part) {
+                array_pop($absolutes);
+            } else {
+                $absolutes[] = $part;
+            }
+        }
+        return implode(DIRECTORY_SEPARATOR, $absolutes);
     }
 
 }
