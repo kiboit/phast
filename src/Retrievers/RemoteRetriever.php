11e2636a767ce723087f0445ce1a7de3a8c549cf
Milko Kosturkov <milko@kiboit.com>
2017-11-10 17:52:01 +0100

    Use firefox user agent signature in remote retriever

--- src/Retrievers/RemoteRetriever.php
+++ src/Retrievers/RemoteRetriever.php
@@ -7,7 +7,14 @@ use Kibo\Phast\ValueObjects\URL;
 class RemoteRetriever implements Retriever {
 
     public function retrieve(URL $url) {
-        return @file_get_contents((string)$url);
+        $ch = curl_init((string)$url);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            ['User-Agent: Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0']
+        );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        return @curl_exec($ch);
     }
 
     public function getLastModificationTime(URL $url) {
