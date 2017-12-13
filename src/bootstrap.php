c5bac9f5cb6e95958e98d4f6ee43df795465c320
Milko Kosturkov <milko@kiboit.com>
2017-12-13 16:48:34 +0100

    Try to load Composer autoloader only if necessary

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+if (!class_exists(\Kibo\Phast\Services\ProxyService::class)) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
 if (!defined('PHAST_CONFIG_FILE')) {
     define('PHAST_CONFIG_FILE', __DIR__ . '/config-default.php');
 }
