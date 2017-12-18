56fefc4b798e5d7ac94e5019781bb3eea28143b5
Milko Kosturkov <milko@kiboit.com>
2017-12-18 15:31:43 +0100

    Report on html filters status

--- src/Diagnostics/SystemDiagnostics.php
+++ src/Diagnostics/SystemDiagnostics.php
@@ -5,6 +5,7 @@ namespace Kibo\Phast\Diagnostics;
 
 
 use Kibo\Phast\Environment\Configuration;
+use Kibo\Phast\Environment\Exceptions\PackageHasNoDiagnosticsException;
 use Kibo\Phast\Environment\Package;
 use Kibo\Phast\Exceptions\RuntimeException;
 
@@ -15,33 +16,48 @@ class SystemDiagnostics {
      * @return Status[]
      */
     public function run(array $config) {
-
-        $runtimeConfig = (new Configuration($config))->toArray();
-
         $results = [];
-        foreach (array_keys($config['images']['filters']) as $filter) {
-            $enabled = isset ($runtimeConfig['images']['filters'][$filter]);
-            $package = Package::fromPackageClass($filter);
-            $diagnostic = $package->getDiagnostics();
-            try {
-                $diagnostic->diagnose($config);
-                $results[] = new Status($package, true, '', $enabled);
-            } catch (RuntimeException $e) {
-                $results[] = new Status($package, false, $e->getMessage(), $enabled);
-            } catch (\Exception $e) {
-                $results[] = new Status(
-                    $package,
-                    false,
-                    sprintf(
-                        'Unknown error: Exception: %s, Message: %s, Code: %s',
-                        get_class($e),
-                        $e->getMessage(),
-                        $e->getCode()
-                    ),
-                    $enabled
-                );
+        foreach ($this->getExaminedItems($config) as $type => $group) {
+            foreach ($group['items'] as $name) {
+                $enabled = in_array($name, $group['runtime']);
+                $package = Package::fromPackageClass($name, $type);
+                try {
+                    $diagnostic = $package->getDiagnostics();
+                    $diagnostic->diagnose($config);
+                    $results[] = new Status($package, true, '', $enabled);
+                } catch (PackageHasNoDiagnosticsException $e) {
+                    $results[] = new Status($package, true, '', $enabled);
+                } catch (RuntimeException $e) {
+                    $results[] = new Status($package, false, $e->getMessage(), $enabled);
+                } catch (\Exception $e) {
+                    $results[] = new Status(
+                        $package,
+                        false,
+                        sprintf(
+                            'Unknown error: Exception: %s, Message: %s, Code: %s',
+                            get_class($e),
+                            $e->getMessage(),
+                            $e->getCode()
+                        ),
+                        $enabled
+                    );
+                }
             }
         }
         return $results;
+    }
+
+    private function getExaminedItems(array $config) {
+        $runtimeConfig = (new Configuration($config))->toArray();
+        return [
+            'HTMLFilter' => [
+                'items' => array_keys($config['documents']['filters']),
+                'runtime' => array_keys($runtimeConfig['documents']['filters'])
+            ],
+            'ImageFilter' => [
+                'items' => array_keys($config['images']['filters']),
+                'runtime' => array_keys($runtimeConfig['images']['filters'])
+            ]
+        ];
     }
 }
