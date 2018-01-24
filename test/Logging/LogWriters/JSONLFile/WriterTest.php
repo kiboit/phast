ac11f11a91678a77226e6fbd6f1362322f141850
Albert Peschar <albert@peschar.net>
2018-01-24 14:21:00 +0100

    Clean up before JSONLFile\WriterTest

--- test/Logging/LogWriters/JSONLFile/WriterTest.php
+++ test/Logging/LogWriters/JSONLFile/WriterTest.php
@@ -13,15 +13,19 @@ class WriterTest extends TestCase {
      */
     public function testWriteMessage($suffix) {
         $dir = sys_get_temp_dir() . '/phast-test';
-        $writer = new Writer($dir, $suffix);
-        $message = new LogEntry(LogLevel::DEBUG, 'The message', ['key' => 'value']);
-        $writer->writeEntry($message);
 
         $fileSuffix = preg_replace('/[^0-9A-Za-z_-]/', '', (string)$suffix);
         if (!empty ($fileSuffix)) {
             $fileSuffix = '-' . $fileSuffix;
         }
         $filename = $dir . '/log' . $fileSuffix .'.jsonl';
+
+        @unlink($filename);
+
+        $writer = new Writer($dir, $suffix);
+        $message = new LogEntry(LogLevel::DEBUG, 'The message', ['key' => 'value']);
+        $writer->writeEntry($message);
+
         $written = file_get_contents($filename);
         $this->assertStringEndsWith("\n", $written);
         $decoded = json_decode(trim($written), true);
