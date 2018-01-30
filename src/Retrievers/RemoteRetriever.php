481572fc0f4d4abe9d29b250f4df41c7ac9c914e
Albert Peschar <albert@peschar.net>
2018-01-30 19:08:25 +0100

    RemoteRetriever: Follow redirects

--- src/Retrievers/RemoteRetriever.php
+++ src/Retrievers/RemoteRetriever.php
@@ -10,7 +10,9 @@ class RemoteRetriever implements Retriever {
         $ch = curl_init((string)$url);
         curl_setopt_array($ch, [
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => ['User-Agent: Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0']
+            CURLOPT_HTTPHEADER => ['User-Agent: Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0'],
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS => 5
         ]);
         $response = @curl_exec($ch);
         if ($response === false) {
