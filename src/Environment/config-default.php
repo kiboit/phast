3357cb5c8de03ba74cdd4cc54bf4937dfcf46e71
Albert Peschar <albert@peschar.net>
2018-02-01 17:07:57 +0100

    Revert "Disable ScriptsRearrangement"
    This reverts commit 494dba596c820ab44fd985c11bac4f39541fa1ea.

--- src/Environment/config-default.php
+++ src/Environment/config-default.php
@@ -48,6 +48,8 @@ return [
 
             \Kibo\Phast\Filters\HTML\DelayedIFrameLoading\Filter::class => [],
 
+            \Kibo\Phast\Filters\HTML\ScriptsRearrangement\Filter::class => [],
+
             \Kibo\Phast\Filters\HTML\ScriptsProxyService\Filter::class => [
                 'urlRefreshTime' => 7200,
                 'match' => [
