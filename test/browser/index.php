e10959d2993cd25dd13df5634abf039b0151e824
Albert Peschar <albert@peschar.net>
2018-02-02 09:32:42 +0100

    Display QUnit fixture

--- test/browser/index.php
+++ test/browser/index.php
@@ -5,6 +5,23 @@
 <meta name="viewport" content="width=device-width">
 <title>Phast Unit Tests</title>
 <link rel="stylesheet" href="qunit.css">
+<style>
+    #qunit-fixture {
+        background: white;
+        position: fixed;
+        width: auto;
+        height: auto;
+        bottom: 10px;
+        right: 10px;
+        top: auto;
+        left: auto;
+    }
+    #qunit-fixture iframe {
+        border: 1px solid #ccc;
+        width: 300px;
+        height: 300px;
+    }
+</style>
 </head>
 <body>
 <div id="qunit"></div>
