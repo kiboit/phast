ca7c4fe79e81fe85cde0455a4b0d5f7e5ca66fdf
Albert Peschar <albert@peschar.net>
2018-02-05 16:36:36 +0100

    Add browser testcase for CSS @import

---
+++ test/browser/tests/stylesheet_import.js
@@ -0,0 +1,21 @@
+test('stylesheet_import.php', function (assert, document) {
+    var h1 = document.getElementsByTagName('h1')[0];
+    var cs = document.defaultView.getComputedStyle(h1, null);
+    var done = assert.async();
+    wait();
+    function wait() {
+        if (document.querySelectorAll('style[data-phast-href]').length > 0) {
+            return setTimeout(wait);
+        }
+        done();
+        assert.equal(cs.backgroundColor, 'rgb(0, 0, 255)', 'The <h1> should have a blue background');
+
+        var importStatements = 0;
+        [].forEach.call(document.getElementsByTagName('style'), function (style) {
+            if (/@import/.test(style.textContent)) {
+                importStatements++;
+            }
+        });
+        assert.equal(importStatements, 0, 'There should be no @import statements in any <style> elements');
+    }
+});
---
+++ test/browser/tests/stylesheet_import.php
@@ -0,0 +1,10 @@
+<?php require '_.php'; ?>
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="../res/stylesheet_import.css">
+</head>
+<body>
+<h1 class="my-class">Hello, World!</h1>
+</body>
+</html>
