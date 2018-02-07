74d9a013257f3f91243784c6635b6fb5469ea948
Albert Peschar <albert@peschar.net>
2018-02-07 17:25:37 +0100

    Add browser testcase for large stylesheet

---
+++ test/browser/tests/stylesheet_large.js
@@ -0,0 +1,17 @@
+test('stylesheet_large.php', function (assert, document) {
+    var h1 = document.getElementsByTagName('h1')[0];
+    var cs = document.defaultView.getComputedStyle(h1, null);
+    var done = assert.async();
+    wait();
+    function wait() {
+        if (document.querySelectorAll('style[data-phast-href]').length > 0) {
+            return setTimeout(wait);
+        }
+        done();
+        assert.equal(cs.backgroundColor, 'rgb(242, 222, 222)', 'The <h1> should have a reddish background');
+
+        var styles = document.querySelectorAll('style');
+        assert.equal(styles.length, 1, 'There should be one style element on the page');
+        assert.ok(styles[0].textContent.length >= 120 * 1000, 'The stylesheet in <style> should be at least 120K')
+    }
+});
---
+++ test/browser/tests/stylesheet_large.php
@@ -0,0 +1,10 @@
+<?php require '_.php'; ?>
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" href="../res/stylesheet_large.css">
+</head>
+<body>
+<h1 class="alert-danger">Hello, World!</h1>
+</body>
+</html>
