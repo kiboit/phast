046a1ba82a694cd1e465642f62ffde2e64a4f45f
Albert Peschar <albert@peschar.net>
2018-02-11 10:43:12 +0100

    Make bootstrap.php work when installed through Composer

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 if (!class_exists('\Kibo\Phast\PhastServices')) {
-    require_once __DIR__ . '/../vendor/autoload.php';
+    if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+        require_once __DIR__ . '/../vendor/autoload.php';
+    } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+        require_once __DIR__ . '/../../../autoload.php';
+    }
 }
 if (!defined('PHAST_CONFIG_FILE')) {
     define('PHAST_CONFIG_FILE', __DIR__ . '/config-user.php');
