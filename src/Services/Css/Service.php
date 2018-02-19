2da3a9499b38944865765faaefc256c880463b9d
Milko Kosturkov <milko@kiboit.com>
2018-02-19 15:21:14 +0100

    Remove unused imports

--- src/Services/Css/Service.php
+++ src/Services/Css/Service.php
@@ -2,11 +2,7 @@
 
 namespace Kibo\Phast\Services\Css;
 
-use Kibo\Phast\Filters\HTML\ImagesOptimizationService\ImageURLRewriter;
-use Kibo\Phast\Retrievers\Retriever;
-use Kibo\Phast\Security\ServiceSignature;
 use Kibo\Phast\Services\ProxyBaseService;
-use Kibo\Phast\Services\ServiceFilter;
 use Kibo\Phast\ValueObjects\Resource;
 
 class Service extends ProxyBaseService {
