7df37a854824ccd30b588e54d0a096bc2c4682b0
Milko Kosturkov <milko@kiboit.com>
2018-02-19 10:06:53 +0100

    Use a fresh DOMDocument for each ScriptsRearrangement iteration

--- bin/profilers/scripts-rearrangement-filter.php
+++ bin/profilers/scripts-rearrangement-filter.php
@@ -1,13 +1,18 @@
 <?php
 
-return call_user_func(function () {
+return call_user_func(function () use ($iterations) {
 
     $html = file_get_contents(__DIR__ . '/../../test/test-app/webcarpet.php');
     $filter = new Kibo\Phast\Filters\HTML\ScriptsRearrangement\Filter();
-    $doc = new \Kibo\Phast\Common\DOMDocument(new \Kibo\Phast\Parsing\HTML\HTMLStream());
-    $doc->loadHTML($html);
 
-    return function () use ($doc, $filter) {
+    for ($i = 0; $i < $iterations; $i++) {
+        $doc = new \Kibo\Phast\Common\DOMDocument(new \Kibo\Phast\Parsing\HTML\HTMLStream());
+        $doc->loadHTML($html);
+        $documents[] = $doc;
+    }
+
+    return function () use (&$documents, $filter) {
+        $doc = array_pop($documents);
         $filter->transformHTMLDOM($doc);
     };
 });
