c246d23a7902b61787cf022e7414c98ea9f5b4e3
Albert Peschar <albert@peschar.net>
2018-02-21 17:45:48 +0100

    Log performance timings to console

--- src/Common/phast-js-env.js
+++ src/Common/phast-js-env.js
@@ -15,6 +15,44 @@ phast.once = function (fn) {
     };
 };
 
+try {
+    document.addEventListener('DOMContentLoaded', logTimings);
+} catch(e) {}
+
+function logTimings() {
+    var t = performance.timing;
+    var m = [];
+    m.push(["Downloading phases:"]);
+    m.push(["  Look up hostname in DNS            + %s ms", fmt(t.domainLookupEnd - t.fetchStart)]);
+    m.push(["  Establish connection               + %s ms", fmt(t.connectEnd - t.domainLookupEnd)]);
+    m.push(["  Send request                       + %s ms", fmt(t.requestStart - t.connectEnd)]);
+    m.push(["  Receive first byte                 + %s ms", fmt(t.responseStart - t.requestStart)]);
+    m.push(["  Download page                      + %s ms", fmt(t.responseEnd - t.responseStart)]);
+    m.push([""]);
+    m.push(["Totals:"])
+    m.push(["  Time to first byte                   %s ms", fmt(t.responseStart - t.fetchStart)]);
+    m.push(["    (since request start)              %s ms", fmt(t.responseStart - t.requestStart)]);
+    m.push(["  Total request time                   %s ms", fmt(t.responseEnd - t.fetchStart)]);
+    m.push(["    (since request start)              %s ms", fmt(t.responseEnd - t.requestStart)]);
+    m.push([" "]);
+    var f = [];
+    var p = [];
+    m.forEach(function (i) {
+        f.push(i.shift());
+        p = p.concat(i);
+    });
+    console.group("[Phast] Performance metrics")
+    console.log.apply(console, [f.join("\n")].concat(p));
+    console.groupEnd()
+    function fmt(v) {
+        v = '' + v;
+        while (v.length < 4) {
+            v = ' ' + v;
+        }
+        return v;
+    }
+}
+
 while (phast.scripts.length) {
     (phast.scripts.shift())();
 }
