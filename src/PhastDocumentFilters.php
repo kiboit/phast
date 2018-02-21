e14e8ba43b561c878df573366cd5f3e99ec67ca1
Albert Peschar <albert@peschar.net>
2018-02-21 17:04:00 +0100

    Output as much as possible before running filters

--- src/PhastDocumentFilters.php
+++ src/PhastDocumentFilters.php
@@ -4,11 +4,25 @@ namespace Kibo\Phast;
 
 use Kibo\Phast\Environment\Configuration;
 use Kibo\Phast\Filters\HTML\Composite\Factory;
+use Kibo\Phast\Filters\HTML\Composite\Filter;
 use Kibo\Phast\HTTP\Request;
 use Kibo\Phast\Logging\Log;
 use Kibo\Phast\Services\ServiceRequest;
 
 class PhastDocumentFilters {
+
+    private $filter;
+
+    private $buffer = '';
+
+    private $startPattern = '~
+        ^ (
+            \s*+ <!doctype\s++html> |
+            \s*+ <html> |
+            \s*+ <head> |
+            \s*+ <!--.*?-->
+        )++
+    ~xsi';
 
     public static function deploy(array $userConfig) {
         $request = ServiceRequest::fromHTTPRequest(Request::fromGlobals());
@@ -24,7 +38,45 @@ class PhastDocumentFilters {
         }
         $filter = (new Factory())->make($runtimeConfig);
         Log::info('Phast deployed!');
-        ob_start([$filter, 'apply']);
+        $handler = new self($filter);
+        $handler->startBuffer();
+    }
+
+    private function __construct(Filter $filter) {
+        $this->filter = $filter;
+    }
+
+    private function startBuffer() {
+        $flags =
+            PHP_OUTPUT_HANDLER_STDFLAGS &
+            ~PHP_OUTPUT_HANDLER_REMOVABLE &
+            ~PHP_OUTPUT_HANDLER_CLEANABLE;
+
+        while (@ob_end_flush());
+        ob_start([$this, 'handleChunk'], 2, $flags);
+        ob_implicit_flush(true);
+    }
+
+    private function handleChunk($chunk, $phase) {
+        if ($this->buffer === null) {
+            return $chunk;
+        }
+        $this->buffer .= $chunk;
+        $output = '';
+        if (preg_match($this->startPattern, $this->buffer, $match)) {
+            $this->buffer = substr($this->buffer, strlen($match[0]));
+            $output .= $match[0];
+        }
+        if ($phase & PHP_OUTPUT_HANDLER_FINAL) {
+            $output .= $this->finalize();
+        }
+        return $output;
+    }
+
+    private function finalize() {
+        $result = $this->filter->apply($this->buffer);
+        $this->buffer = null;
+        return $result;
     }
 
 }
