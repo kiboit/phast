f6b9920db67eb3bd75c21dd4b33ae60a467b4e8f
Albert Peschar <albert@peschar.net>
2018-02-23 09:19:37 +0100

    Remove spec tests that don't work in IE

--- test/browser/tests/spec_dynamic_scripts_delay_onload.js
+++
@@ -1,5 +0,0 @@
-test('spec_dynamic_scripts_delay_onload.php', function (assert, document) {
-    wait(assert, function () {
-        return typeof document.defaultView.done !== 'undefined';
-    });
-}, false);
