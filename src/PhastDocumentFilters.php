1a66137cac8efbb89dff7a813e56f1b729d0dac3
Milko Kosturkov <milko@kiboit.com>
2018-02-23 17:56:51 +0100

    Remove unused use statement

--- src/PhastDocumentFilters.php
+++ src/PhastDocumentFilters.php
@@ -5,7 +5,6 @@ namespace Kibo\Phast;
 use Kibo\Phast\Common\OutputBufferHandler;
 use Kibo\Phast\Environment\Configuration;
 use Kibo\Phast\Filters\HTML\Composite\Factory;
-use Kibo\Phast\Filters\HTML\Composite\Filter;
 use Kibo\Phast\HTTP\Request;
 use Kibo\Phast\Logging\Log;
 use Kibo\Phast\Services\ServiceRequest;
