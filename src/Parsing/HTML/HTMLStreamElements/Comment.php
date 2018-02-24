e4a1274c24aacd80eda2ba753a97d02716f575e3
Albert Peschar <albert@peschar.net>
2018-02-24 07:01:01 +0100

    Not all whitespace is created equal

--- src/Parsing/HTML/HTMLStreamElements/Comment.php
+++ src/Parsing/HTML/HTMLStreamElements/Comment.php
@@ -7,7 +7,7 @@ namespace Kibo\Phast\Parsing\HTML\HTMLStreamElements;
 class Comment extends Element {
 
     public function isIEConditional() {
-        return substr($this->originalString, 4, 4) === '[if ';
+        return (bool) preg_match('/^<!--\[if\s/', $this->originalString);
     }
 
 }
