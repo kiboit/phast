5d54b1473bde9cd2ab29bca2447008d94ebe0cff
Albert Peschar <albert@peschar.net>
2018-02-24 06:38:02 +0100

    Clean up JSDetectorTrait

--- src/Filters/HTML/Helpers/JSDetectorTrait.php
+++ src/Filters/HTML/Helpers/JSDetectorTrait.php
@@ -6,16 +6,15 @@ use Kibo\Phast\Parsing\HTML\HTMLStreamElements\Tag;
 
 trait JSDetectorTrait {
 
-
     /**
      * @param Tag $element
-     * @return bool|false|int
+     * @return bool
      */
     private function isJSElement(Tag $element) {
         if (!$element->hasAttribute('type')) {
             return true;
         }
-        return preg_match('~^(text|application)/javascript(;|$)~i', $element->getAttribute('type'));
+        return (bool) preg_match('~^(text|application)/javascript(;|$)~i', $element->getAttribute('type'));
     }
 
 }
