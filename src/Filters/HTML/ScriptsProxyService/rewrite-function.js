288892f35257d8cc165f772dd215af8ef74d4c0a
Albert Peschar <albert@peschar.net>
2018-03-07 07:29:09 +0100

    Simply set the src after injecting the script, instead of ID stuff

--- src/Filters/HTML/ScriptsProxyService/rewrite-function.js
+++ src/Filters/HTML/ScriptsProxyService/rewrite-function.js
@@ -39,106 +39,31 @@ function checkWhitelist(value) {
 function overrideDOMMethod(name) {
     var original = Element.prototype[name];
     Element.prototype[name] = function () {
-        processNode(arguments[0]);
-        return original.apply(this, arguments);
+        var postprocess = processNode(arguments[0]);
+        var result = original.apply(this, arguments);
+        postprocess();
+        return result;
     };
 }
 
 function processNode(el) {
-    if (!el) {
-        return;
+    if (!el
+        || el.nodeType !== Node.ELEMENT_NODE
+        || el.tagName !== 'SCRIPT'
+        || !urlPattern.test(el.src)
+        || el.src.substr(0, config.serviceUrl.length) === config.serviceUrl
+        || !checkWhitelist(el.src)
+    ) {
+        return function () {};
     }
-    if (el.nodeType !== Node.ELEMENT_NODE) {
-        return;
-    }
-    if (el.tagName !== 'SCRIPT') {
-        return;
-    }
-    if (!urlPattern.test(el.src)) {
-        return;
-    }
-    if (el.src.substr(0, config.serviceUrl.length) == config.serviceUrl) {
-        return;
-    }
-    if (!checkWhitelist(el.src)) {
-        return;
-    }
-    var id = getScriptId(el.src);
-    el.setAttribute('data-phast-proxied-script', id);
+
+    var originalSrc = el.src;
+
     el.src = config.serviceUrl +
-        '&src=' + encodeURIComponent(el.src) +
-        '&cacheMarker=' + encodeURIComponent(cacheMarker) +
-        '&id=' + id;
-}
+        '&src=' + encodeURIComponent(originalSrc) +
+        '&cacheMarker=' + encodeURIComponent(cacheMarker);
 
-function getScriptId(src) {
-    var hash = murmurhash3_32_gc(src);
-    ids[hash] = ids[hash] || 0;
-    var count = ++ids[hash];
-    return 'd-' + hash + '-' + count;
-}
-
-/**
- * JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
- *
- * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
- * @see http://github.com/garycourt/murmurhash-js
- * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
- * @see http://sites.google.com/site/murmurhash/
- *
- * @param {string} key ASCII only
- * @param {number} seed Positive integer only
- * @return {number} 32-bit positive integer hash
- */
-
-function murmurhash3_32_gc(key, seed) {
-    var remainder, bytes, h1, h1b, c1, c1b, c2, c2b, k1, i;
-
-    remainder = key.length & 3; // key.length % 4
-    bytes = key.length - remainder;
-    h1 = seed;
-    c1 = 0xcc9e2d51;
-    c2 = 0x1b873593;
-    i = 0;
-
-    while (i < bytes) {
-        k1 =
-            ((key.charCodeAt(i) & 0xff)) |
-            ((key.charCodeAt(++i) & 0xff) << 8) |
-            ((key.charCodeAt(++i) & 0xff) << 16) |
-            ((key.charCodeAt(++i) & 0xff) << 24);
-        ++i;
-
-        k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
-        k1 = (k1 << 15) | (k1 >>> 17);
-        k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
-
-        h1 ^= k1;
-        h1 = (h1 << 13) | (h1 >>> 19);
-        h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
-        h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
-    }
-
-    k1 = 0;
-
-    switch (remainder) {
-        case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
-        case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
-        case 1: k1 ^= (key.charCodeAt(i) & 0xff);
-
-            k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
-            k1 = (k1 << 15) | (k1 >>> 17);
-            k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
-            h1 ^= k1;
-    }
-
-    h1 ^= key.length;
-
-    h1 ^= h1 >>> 16;
-    h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
-    h1 ^= h1 >>> 13;
-    h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
-    h1 ^= h1 >>> 16;
-
-    return h1 >>> 0;
+    return function () {
+        el.src = originalSrc;
+    };
 }
