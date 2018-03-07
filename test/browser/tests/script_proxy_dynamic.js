ce427afb7926118dc417ed6c8b6e4155aa1663a3
Albert Peschar <albert@peschar.net>
2018-03-07 09:30:58 +0100

    Fix timing issue with browser test

--- test/browser/tests/script_proxy_dynamic.js
+++ test/browser/tests/script_proxy_dynamic.js
@@ -1,10 +1,14 @@
 test('script_proxy_dynamic.php', function (assert, document) {
-    var h1s = document.getElementsByTagName('h1');
-    assert.equal(h1s.length, 1, "There should be one <H1> element on the page");
-    assert.equal(h1s[0].textContent, 'Hello, World!', "The <H1> should say 'Hello, World!'");
+    wait(assert, function () {
+        return document.getElementsByTagName('h1').length > 0;
+    }, function () {
+        var h1s = document.getElementsByTagName('h1');
+        assert.equal(h1s.length, 1, "There should be one <H1> element on the page");
+        assert.equal(h1s[0].textContent, 'Hello, World!', "The <H1> should say 'Hello, World!'");
 
-    var scripts = document.getElementsByTagName('script');
-    assert.ok(scripts.length >= 1, "There should be one or more <SCRIPT> elements on the page");
-    assert.ok(/script_proxy\.js/.test(scripts[0].src), "The first <SCRIPT> src should contain the name of the script");
-    assert.ok(!/phast\.php/.test(scripts[0].src), "The first <SCRIPT> src should not contain phast.php");
+        var scripts = document.getElementsByTagName('script');
+        assert.ok(scripts.length >= 1, "There should be one or more <SCRIPT> elements on the page");
+        assert.ok(/script_proxy\.js/.test(scripts[0].src), "The first <SCRIPT> src should contain the name of the script");
+        assert.ok(!/phast\.php/.test(scripts[0].src), "The first <SCRIPT> src should not contain phast.php");
+    });
 });
