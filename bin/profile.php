71181df653cd62d37bbfc83c406494703ed5778f
Albert Peschar <albert@peschar.net>
2018-03-18 07:47:11 +0100

    Add profiler for CSS parsing

--- bin/profile.php
+++ bin/profile.php
@@ -1,14 +1,14 @@
 <?php
 
 if (PHP_SAPI != 'cli') {
-    echo "This script must be run from the command line.\n";
+    fwrite(STDERR, "This script must be run from the command line.\n");
     exit(1);
 }
 
 if (sizeof($argv) != 3) {
-    echo "Run a script that returns in callable inside a profiling loop for\n";
-    echo "the specified number of iterations.\n\n";
-    echo "Usage: {$argv[0]} <script> <iterations>\n";
+    fwrite(STDERR, "Run a script that returns in callable inside a profiling loop for\n");
+    fwrite(STDERR, "the specified number of iterations.\n\n");
+    fwrite(STDERR, "Usage: {$argv[0]} <script> <iterations>\n");
     exit(1);
 }
 
@@ -19,14 +19,29 @@ $iterations = (int)$argv[2];
 
 $callback = require_once $file;
 if (!is_callable($callback)) {
-    echo "$file did not return a callable\n";
+    fwrite(STDERR, "$file did not return a callable\n");
     exit(1);
 }
 
-tideways_xhprof_enable();
+$profile = function_exists('tideways_xhprof_enable');
 
+if (!$profile) {
+    fwrite(STDERR, "Warning: Tideways/XHProf extension is missing; not profiling...\n");
+}
+
+if ($profile) {
+    tideways_xhprof_enable();
+}
+
+$timeStart = microtime(true);
 for ($i = 0; $i < $iterations; $i++) {
     $callback();
 }
+$timeEnd = microtime(true);
 
-echo serialize(tideways_xhprof_disable());
+$timeElapsed = $timeEnd - $timeStart;
+fprintf(STDERR, "Ran %d iterations in %.4fs (%.4fs/iteration)\n", $iterations, $timeElapsed, $timeElapsed/$iterations);
+
+if ($profile) {
+    echo serialize(tideways_xhprof_disable());
+}
---
+++ bin/profilers/css-optimizer.php
@@ -0,0 +1,18 @@
+<?php
+
+use Kibo\Phast\Cache\File\Cache;
+use Kibo\Phast\Filters\HTML\CSSInlining\Optimizer;
+
+return call_user_func(function () {
+    $css = file_get_contents(__DIR__ . '/../../test/resources/large.css');
+
+    $mocker = new PHPUnit_Framework_MockObject_Generator();
+    $cache = $mocker->getMock(Cache::class, [], [], '', false);
+
+    $optimizer = new Optimizer(new \ArrayIterator([]), $cache);
+
+    return Closure::bind(function () use ($css) {
+        $this->parseCSS($css);
+    }, $optimizer, $optimizer);
+});
+
