451756d55eb8205a03bafeb96969e195a8beda19
Albert Peschar <albert@peschar.net>
2018-03-20 17:46:24 +0100

    Wait for IFrame to load in test

--- test/browser/tests/iframe.js
+++ test/browser/tests/iframe.js
@@ -1,4 +1,8 @@
 test('iframe.php', function (assert, document) {
-    assert.equal(document.defaultView.iframeLoads, 1, "The IFrame should have been loaded once");
-    assert.ok(document.defaultView.srcWasChanged, "Initially, the IFrame src should have had an src of about:blank");
+    wait(assert, function () {
+        return document.defaultView.iframeLoads > 0;
+    }, function () {
+        assert.equal(document.defaultView.iframeLoads, 1, "The IFrame should have been loaded once");
+        assert.ok(document.defaultView.srcWasChanged, "Initially, the IFrame src should have had an src of about:blank");
+    });
 });
--- test/browser/tests/iframe_js.js
+++ test/browser/tests/iframe_js.js
@@ -1,4 +1,12 @@
 test('iframe_js.php', function (assert, document) {
-    assert.equal(document.defaultView.iframeLoads, 1, "The IFrame should have been loaded once");
-    assert.ok(document.defaultView.srcWasChanged, "Initially, the IFrame src should have had an src of about:blank");
+    wait(assert, function () {
+        return document.defaultView.iframeLoads > 0;
+    }, function () {
+        var done = assert.async();
+        setTimeout(function () {
+            done();
+            assert.equal(document.defaultView.iframeLoads, 1, "The IFrame should have been loaded once");
+            assert.ok(document.defaultView.srcWasChanged, "Initially, the IFrame src should have had an src of about:blank");
+        }, 50);
+    });
 });
