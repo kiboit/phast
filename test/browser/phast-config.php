fd510a075abfb7e47384cbc18c71572dfd87952c
Albert Peschar <albert@peschar.net>
2018-04-13 07:32:22 +0300

    Fix tests on main domain

--- test/browser/phast-config.php
+++ test/browser/phast-config.php
@@ -1,4 +1,4 @@
 <?php
 return [
-    'servicesUrl' => preg_replace('~^(.*/browser)/.*~si', '$1/', $_SERVER['PHP_SELF']) . 'phast.php',
+    'servicesUrl' => preg_replace('~^(.*/browser)?/.*~si', '$1/', $_SERVER['PHP_SELF']) . 'phast.php',
 ];
