e9e0640b2350ce7631014e7514250140e80f3b62
Milko Kosturkov <milko@kiboit.com>
2018-06-04 16:55:13 +0300

    Refactor - extract a factory for http client

---
+++ src/HTTP/ClientFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Kibo\Phast\HTTP;
+
+
+use Kibo\Phast\Exceptions\RuntimeException;
+
+class ClientFactory {
+
+    const CONFIG_KEY = 'httpClient';
+
+    /**
+     * @param array $config
+     * @return Client
+     */
+    public function make(array $config) {
+        $spec = $config[self::CONFIG_KEY];
+
+        if (is_callable($spec)) {
+            $client = $spec();
+        } elseif (class_exists($spec)) {
+            $client = new $spec();
+        } else {
+            throw new RuntimeException(self::CONFIG_KEY . " config value must be either callable or a class name");
+        }
+
+        return $client;
+    }
+
+}
--- src/Retrievers/RemoteRetrieverFactory.php
+++ src/Retrievers/RemoteRetrieverFactory.php
@@ -2,24 +2,14 @@
 
 namespace Kibo\Phast\Retrievers;
 
-use Kibo\Phast\Exceptions\RuntimeException;
+use Kibo\Phast\HTTP\ClientFactory;
 
 class RemoteRetrieverFactory {
 
-    const CONFIG_KEY = 'httpClient';
-
     public function make(array $config) {
-        $spec = $config[self::CONFIG_KEY];
-
-        if (is_callable($spec)) {
-            $client = $spec();
-        } elseif (class_exists($spec)) {
-            $client = new $spec;
-        } else {
-            throw new RuntimeException(self::CONFIG_KEY . " config value must be either callable or a class name");
-        }
-
-        return new RemoteRetriever($client);
+        return new RemoteRetriever(
+            (new ClientFactory())->make($config)
+        );
     }
 
 }
