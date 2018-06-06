c396fa8d37754b91fdc83e5366db906a842b873d
Milko Kosturkov <milko@kiboit.com>
2018-06-06 12:44:06 +0300

    Add comments to the Kibo\Phast\HTTP\Client interface

--- src/HTTP/Client.php
+++ src/HTTP/Client.php
@@ -7,17 +7,21 @@ use Kibo\Phast\ValueObjects\URL;
 interface Client {
 
     /**
+     * Retrieve a URL using the GET HTTP method
+     *
      * @param URL $url
-     * @param array $headers
+     * @param array $headers - headers to send in headerName => headerValue format
      * @return Response|false
      * @throws \Exception
      */
     public function get(URL $url, array $headers = []);
 
     /**
+     * Send data to a URL using the POST HTTP method
+     *
      * @param URL $url
-     * @param array|string $data
-     * @param array $headers
+     * @param array|string $data - if array, it will be encoded as form data, if string - will be sent as is
+     * @param array $headers - headers to send in headerName => headerValue format
      * @return Response|false
      * @throws \Exception
      */
