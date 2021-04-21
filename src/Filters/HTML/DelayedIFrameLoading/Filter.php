dc9314dc30e550ff691847bb013ac33909898dca
Albert Peschar <albert@peschar.net>
2021-04-21 13:34:18 +0300

    Use native IFrame lazy loading

--- src/Filters/HTML/DelayedIFrameLoading/Filter.php
+++ src/Filters/HTML/DelayedIFrameLoading/Filter.php
@@ -1,43 +1,22 @@
 <?php
-
 namespace Kibo\Phast\Filters\HTML\DelayedIFrameLoading;
 
 use Kibo\Phast\Filters\HTML\BaseHTMLStreamFilter;
 use Kibo\Phast\Logging\LoggingTrait;
 use Kibo\Phast\Parsing\HTML\HTMLStreamElements\Tag;
-use Kibo\Phast\ValueObjects\PhastJavaScript;
 
 class Filter extends BaseHTMLStreamFilter {
     use LoggingTrait;
-
-    protected $addScript = false;
-
-    private $ignoredUrlPattern = '~
-        ^about: |
-        ^data:
-    ~ix';
 
     protected function isTagOfInterest(Tag $tag) {
         return $tag->getTagName() == 'iframe'
                && $tag->hasAttribute('src');
     }
 
-    protected function handleTag(Tag $iframe) {
-        $src = trim($iframe->getAttribute('src'));
-        if (preg_match($this->ignoredUrlPattern, $src)) {
-            yield $iframe;
-            return;
+    protected function handleTag(Tag $tag) {
+        if (!$tag->hasAttribute('loading')) {
+            $tag->setAttribute('loading', 'lazy');
         }
-        $this->logger()->info('Delaying iframe {src}', ['src' => $src]);
-        $iframe->setAttribute('data-phast-src', $src);
-        $iframe->setAttribute('src', 'about:blank');
-        $this->addScript = true;
-        yield $iframe;
-    }
-
-    protected function afterLoop() {
-        if ($this->addScript) {
-            $this->context->addPhastJavaScript(PhastJavaScript::fromFile(__DIR__ . '/iframe-loader.js'));
-        }
+        yield $tag;
     }
 }
--- test/php/Filters/HTML/DelayedIFrameLoading/FilterTest.php
+++ test/php/Filters/HTML/DelayedIFrameLoading/FilterTest.php
@@ -11,62 +11,14 @@ class FilterTest extends HTMLFilterTestCase {
     }
 
     public function testRewriting() {
-        $regularFrame = $this->makeMarkedElement('iframe');
-        $rewrittenFrame = $this->makeMarkedElement('iframe');
-        $rewrittenFrame->setAttribute('src', 'http://kibo.test/index.php');
-        $this->body->appendChild($regularFrame);
-        $this->body->appendChild($rewrittenFrame);
-
-        $this->applyFilter();
-
-        $regularFrame = $this->getMatchingElement($regularFrame);
-        $rewrittenFrame = $this->getMatchingElement($rewrittenFrame);
-
-        $this->assertFalse($regularFrame->hasAttribute('src'));
-        $this->assertFalse($regularFrame->hasAttribute('data-phast-src'));
-        $this->assertTrue($rewrittenFrame->hasAttribute('src'));
-        $this->assertTrue($rewrittenFrame->hasAttribute('data-phast-src'));
-        $this->assertEquals('about:blank', $rewrittenFrame->getAttribute('src'));
-        $this->assertEquals('http://kibo.test/index.php', $rewrittenFrame->getAttribute('data-phast-src'));
-
-        $this->assertHasCompiled('DelayedIFrameLoading/iframe-loader.js');
-    }
-
-    /** @dataProvider dontRewriteData */
-    public function testDontRewrite($url) {
-        $iframe = $this->insertIframeWithSrc($url);
+        $iframe = $this->makeMarkedElement('iframe');
+        $iframe->setAttribute('src', 'http://example.com');
+        $this->body->appendChild($iframe);
 
         $this->applyFilter();
 
         $iframe = $this->getMatchingElement($iframe);
-        $this->assertEquals($url, $iframe->getAttribute('src'));
-        $this->assertFalse($iframe->hasAttribute('data-phast-src'));
-    }
 
-    private function insertIframeWithSrc($url) {
-        $iframe = $this->makeMarkedElement('iframe');
-        $iframe->setAttribute('src', $url);
-        $this->body->appendChild($iframe);
-        return $iframe;
-    }
-
-    public function testTrimSrc() {
-        $input = '<iframe src=" http://nu.nl ">';
-
-        $html = $this->applyFilter($input, true);
-
-        $this->assertContains('"http://nu.nl"', $html);
-    }
-
-    public function dontRewriteData() {
-        yield ['about:blank'];
-        yield ['data:abc,def'];
-    }
-
-    public function testNotAppendingScript() {
-        $frame = $this->dom->createElement('iframe');
-        $this->body->appendChild($frame);
-        $this->applyFilter();
-        $this->assertHasNotCompiledScripts();
+        $this->assertEquals('lazy', $iframe->getAttribute('loading'));
     }
 }
