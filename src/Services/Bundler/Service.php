7b564a52e3e11f161e89343c5516663c0c44b832
Albert Peschar <albert@peschar.net>
2022-11-18 16:14:47 +0000

    Revert "Use text/plain content type for bundler response"
    This reverts commit 5299405d6b6c4841e5263e4eb351688cd575708a.

--- src/Services/Bundler/Service.php
+++ src/Services/Bundler/Service.php
@@ -65,7 +65,7 @@ class Service {
      */
     public function serve(ServiceRequest $request) {
         $response = new Response();
-        $response->setHeader('Content-Type', 'text/plain');
+        $response->setHeader('Content-Type', 'application/json');
         $response->setHeader('X-Robots-Tag', 'none');
         $response->setContent($this->streamResponse($request));
         return $response;
